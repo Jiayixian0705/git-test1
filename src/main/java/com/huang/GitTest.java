9633254fde71bed721c3e8b0c0a5ed37023bcd80
huanghui <huanghui@qq.com>
2022-08-23 17:14:00 +0800

    first commit

---
+++ src/main/java/com/huang/GitTest.java
@@ -0,0 +1,14 @@
+package com.huang;
+
+/**
+ * @Author Huang
+ * @ClassName GitTest
+ * @Description
+ * @Date 2022/8/23 16:58
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("gitTest");
+    }
+}
