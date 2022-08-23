e24770e10c5416f89ed7dff09b06ac5d30adb2c1
Jiayixian47 <101186717+Jiayixian47@users.noreply.github.com>
2022-08-23 17:49:43 +0800

    pull commit

--- src/main/java/com/huang/GitTest.java
+++ src/main/java/com/huang/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("gitTest-master");
         System.out.println("gitTest-hot-fix");
         System.out.println("push commit");
+        System.out.println("pull commit");
     }
 }
