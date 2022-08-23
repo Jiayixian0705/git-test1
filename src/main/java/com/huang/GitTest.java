9adcada82fcc983ab3c011555fd9d676e5fcf87b
huanghui <huanghui@qq.com>
2022-08-23 17:23:29 +0800

    hot-fix first commit

--- src/main/java/com/huang/GitTest.java
+++ src/main/java/com/huang/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("gitTest");
         System.out.println("gitTest2");
+        System.out.println("gitTest3");
     }
 }
